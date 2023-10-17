2df439e21c7a2f293ee51b6355979d6a03e8ece0
Alessandro <sandroscro@libero.it>
2023-10-17 12:23:42 +0200

    Metodo aggiunto

--- MyClass.java
+++ MyClass.java
@@ -1,8 +1,11 @@
 public class MyClass {
     public static void main(String args[]) {
       int x=4;
-      for (int i=0; i<=10;i++){
-          System.out.println(x*i);
-      }
+      tabellina(x);
+    }
+    public static void tabellina(int x){
+        for (int i=0; i<=10;i++){
+            System.out.print(x*i+"   ");
+        }
     }
 }
